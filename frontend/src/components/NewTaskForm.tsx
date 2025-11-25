176d6514dcb958af1a08f853b48ff6dd6e0f9420
OhMyLaia <ohmylaia@live.com>
2025-11-25 19:33:28 +0100

    added custom penalty input if user chooses custom penalty

--- frontend/src/components/NewTaskForm.tsx
+++ frontend/src/components/NewTaskForm.tsx
@@ -30,7 +30,7 @@ function NewTaskForm() {
     return (
         <div className='p-4 flex flex-col justify-start items-center w-full text-indigo-900'>
             <h2 className='text-center lato-regular text-3xl'>New custom task</h2>
-            <div className='bg-emerald-50/80 h-1/3 w-3/4 shadow-lg m-5 rounded'>
+            <div className='bg-emerald-50/80 h-1/3 w-3/4 shadow-lg m-5 rounded-xl'>
                 <form className='flex flex-col gap-y-4 w-full p-5'>
 
                     <label htmlFor="task_id" className="font-medium"> Select a task </label>
@@ -99,11 +99,24 @@ function NewTaskForm() {
                         onChange={(newValue) => setPenalty(newValue)}
                     />
 
+                    {penalty === penaltyOptions[0] ? <div>
+                        <label htmlFor="task_id" className="font-thin"> Can't find it? Create a custom penalty </label>
+                        <input
+                            className="w-full border border-emerald-400 rounded-xl bg-white p-3"
+                            type="text"
+                            id="task_id"
+                            name="name"
+                        // value={formData.name}
+                        // onChange={handleChange}
+                        />
+                    </div>
+                        : ""}
+
                     <PrimaryButton
                         // isLoading={isLoading}
                         type={"submit"}
                         children={"Submit"}
-                        className={`${"bg-indigo-900 text-white"}`}
+                        className={`${"bg-indigo-900 text-white hover:bg-emerald-400"}`}
                     />
                 </form>
             </div>
